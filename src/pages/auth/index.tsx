556d968caf9449d4f92cba0e1699b115c595c9e7
samani21 <76203958+samani21@users.noreply.github.com>
2025-05-26 17:16:01 +0800

    Update index.tsx

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -57,7 +57,7 @@ const AuthLayout: React.FC<AuthLayoutProps> = ({ children, mode }) => {
             <NavAuth>
                 <HeaderContainer>
                     <HeaderLeft>
-                        <LogoHeader src='/logo/shopee.png' />
+                        <LogoHeader src='/logo/Shopee.png' />
                         <TextLogin>{mode === 'register' ? "Daftar" : "Log in"}</TextLogin>
                     </HeaderLeft>
                     <TextHelper>Butuh bantuan?</TextHelper>
