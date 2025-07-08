2fcd9bdace93f1559f62b7d1a04ae5e69a09b927
Menulis Aplikasi <menulisaplikasi@gmail.com>
2025-07-08 11:26:52 +0800

    Memperbaiki tampilan user prfile/ dashboard

--- src/components/userProfile/DesktopSidebar.tsx
+++ src/components/userProfile/DesktopSidebar.tsx
@@ -62,11 +62,13 @@ const DesktopSidebar = () => {
         }
     }, [])
     return (
-        <aside className="w-55 mr-[40px]  hidden md:flex flex-col rounded-br-lg rounded-bl-lg bg-white border border-gray-300">
-            <div className="flex items-center justify-center gap-3 p-3 border-b border-gray-300">
-                <div >
-                    <img src={user?.image ?? "https://placehold.co/40x40/e2e8f0/333?text=Z"} alt="User Avatar" className="w-20 h-20 rounded-full" />
-                    <h4 className=" text-lg mt-3 text-center">{user?.name ?? "Nama Anda"}</h4>
+        <aside className="w-[230px] mr-[40px] h-[563px]  hidden md:flex flex-col rounded-br-lg rounded-bl-lg bg-white border border-gray-300">
+            <div className="flex items-center justify-center gap-3 p-3">
+                <div>
+                    <div className='flex justify-center items-center'>
+                        <img src={user?.image ?? "https://placehold.co/40x40/e2e8f0/333?text=Z"} alt="User Avatar" className="w-[49px] h-[49px] rounded-full" />
+                    </div>
+                    <h4 className="text-[16px] font-semibold mt-1 text-center">{user?.name ?? "Nama Anda"}</h4>
                 </div>
             </div>
             <nav className="flex-grow p-2">
@@ -76,7 +78,7 @@ const DesktopSidebar = () => {
                             <button
                                 onClick={() => router.push(item?.url)}
                                 className={`w-full flex items-center gap-3 text-left py-2 px-3 rounded-lg transition-colors ${router.pathname === item.url
-                                    ? 'text-dark-600 font-bold'
+                                    ? 'text-white font-bold bg-[#7952B3] shadow-lg'
                                     : 'text-dark-600 hover:bg-gray-100'
                                     }`}
                             >
@@ -87,14 +89,14 @@ const DesktopSidebar = () => {
                     ))}
                 </ul>
             </nav>
-            <div className="mt-4 pt-4 px-3 p-2">
+            <div className="mt-4 px-4 py-6">
                 {/* <button className="w-full flex items-center gap-3 text-left py-2 px-3 rounded-lg transition-colors text-gray-600 hover:bg-gray-100 text-red-600" onClick={handleLogout}>
                     <LogoutIcon className="w-5 h-5" />
                     <span>Logout</span>
                 </button> */}
                 <div className='flex justify-center itmes-center'>
-                    <button className="w-full border border-gray-300 p-2 rounded-sm" onClick={handleLogout}>
-                        <span className='font-bold'>Logout</span>
+                    <button className="w-full border border-[#AAAAAA] p-2 rounded-[10px]" onClick={handleLogout}>
+                        <span className='font-semibold text-[14px]'>Logout</span>
                     </button>
                 </div>
             </div>
